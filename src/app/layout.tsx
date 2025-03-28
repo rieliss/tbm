a8a4d4eab255236a3391ef4fe7ebdf8980b0d72c
rabitch <133730089+rieliss@users.noreply.github.com>
2025-03-28 22:56:07 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Alexander Alexiev",
+  title: "Gene",
   description:
     "Personal website of Alexander Alexiev - Engineering Science student at the University of Toronto",
 };
